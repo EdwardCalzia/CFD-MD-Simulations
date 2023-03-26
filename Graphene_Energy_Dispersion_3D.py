fbd05819f0a228d7b1afd200462cdda7c9ce3376
Edward Calzia <84807135+EdwardCalzia@users.noreply.github.com>
2023-03-26 19:31:37 +0100

    Create Graphene_Energy_Dispersion_3D.py

---
+++ Graphene_Energy_Dispersion_3D.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Lattice constant (in Angstroms)
+a = 2.46
+
+# On-site energy (in eV)
+epsilon_0 = 0
+
+# Hopping parameter (in eV)
+t = 2.7
+
+# Define the reciprocal lattice vectors
+b1 = (2*np.pi/a) * np.array([1, -1/np.sqrt(3)])
+b2 = (2*np.pi/a) * np.array([1, 1/np.sqrt(3)])
+
+# Define the k-vector range in the first Brillouin zone
+k_min = -np.pi/(3*a)
+k_max = np.pi/(3*a)
+nk = 500
+kx, ky = np.meshgrid(np.linspace(k_min, k_max, nk), np.linspace(k_min, k_max, nk))
+k_vec = np.stack((kx.ravel(), ky.ravel()), axis=1)
+
+# Define the Fourier transform of the hopping term
+def f_k(k):
+    return t * (np.exp(-1j*np.dot(k,b1)) + np.exp(-1j*np.dot(k,b2)) + np.exp(1j*np.dot(k,b1-b2)) + np.exp(1j*np.dot(k,b2-b1)) + np.exp(1j*np.dot(k,b1+b2)) + np.exp(-1j*np.dot(k,b1+b2)))
+
+# Calculate the energy dispersion relation
+E = np.sqrt((epsilon_0**2) + np.abs(f_k(k_vec).reshape(nk,nk))**2 + 2*epsilon_0*np.sqrt(1+4*np.cos(np.sqrt(3)*ky*a/2)**2+4*np.cos(np.sqrt(3)*ky*a/2)*np.cos(3*kx*a/2)))
+
+# Plot the energy dispersion relation
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('$k_x$ ($\mathrm{\AA}^{-1}$)')
+ax.set_ylabel('$k_y$ ($\mathrm{\AA}^{-1}$)')
+ax.set_zlabel('Energy (eV)')
+ax.set_title('Graphene Energy Dispersion Relation')
+ax.plot_surface(kx, ky, E, cmap='viridis', linewidth=0, antialiased=False)
+plt.show()
